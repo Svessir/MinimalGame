47b116afd9146b436ac26c54a57646c1755d465b
sverrir <sverrir1993@gmail.com>
2017-01-22 11:45:55 +0000

    original commit

---
+++ Assets/AgentController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class AgentController : MonoBehaviour {
+    private CharacterController characterController;
+
+    [SerializeField]
+    private Transform camera;
+
+    [SerializeField]
+    private float speed = 2f;
+
+    [SerializeField]
+    private LayerMask mask;
+
+    [SerializeField]
+    private Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        characterController = GetComponent<CharacterController>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 forwardDirection = camera.forward;
+        Vector3 rightDirection = camera.right;
+        forwardDirection.Scale(new Vector3(1, 0, 1));
+        rightDirection.Scale(new Vector3(1, 0, 1));
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 aimDirection = transform.forward;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask, QueryTriggerInteraction.Collide))
+        {
+            aimDirection = (hit.point - transform.position);
+            aimDirection.Scale(new Vector3(1,0,1));
+            Debug.DrawRay(transform.position, hit.point - transform.position, Color.red);
+            Debug.Log(hit.collider.gameObject.tag);
+            transform.forward = aimDirection.normalized;
+        }
+
+        Vector3 direction = (vertical * forwardDirection + horizontal * rightDirection).normalized;
+        Debug.DrawRay(transform.position, direction, Color.blue);
+        characterController.Move(direction * speed * Time.deltaTime);
+        animator.SetFloat("Speed", (direction * speed).magnitude);
+
+        float angle = Vector3.Angle(direction, aimDirection);
+        Vector3 cross = Vector3.Cross(direction, aimDirection);
+        angle *= cross.y < 0 ? -1 : 1;
+        //Debug.Log(cross);
+        Debug.Log(angle);
+        animator.SetFloat("Angle", angle);
+    }
+}
---
+++ Assets/CameraFollowScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 distanceVector;
+    [SerializeField]
+    private Transform target;
+    private Vector3 currentVector;
+
+    public void Awake()
+    {
+        currentVector = transform.position - target.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 targetPosition = target.TransformPoint(distanceVector);
+        transform.LookAt(target);
+        transform.position = target.position + currentVector;
+    }
+}
